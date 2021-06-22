2ab9178ed9f7d355d12e078e06392d09f07fe6cb
Alexander Botwinick <alexbotwinick@gmail.com>
2021-06-22 11:54:08 +0200

    expense item component implementation

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,22 @@
 // import React from 'react';
 import './ExpenseItem.css'; // tells build process to consider this css
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+
   return (
     <div className='expense-item'>
-      <div>June 15th, 2021</div>
+      <div>
+        <div className='date-month'>{month}</div>
+        <div className='date-year'>{year}</div>
+        <div className='date-day'>{day}</div>
+      </div>
+
       <div className='expense-item__description'>
-        <h2>Emma Matras</h2>
-        <div className='expense-item__price'>800€</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>{`${props.amount}€`}</div>
       </div>
     </div>
   );

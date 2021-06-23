8dbfd5b3ba9ce055d583b468d41bc4368f6b03df
Alexander Botwinick <alexbotwinick@gmail.com>
2021-06-23 16:48:48 +0200

    added state to save filterYear and ExpensesFilter

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import ExpensesFilter from './ExpensesFilter';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 
 import './ExpenseList.css';
 
 const ExpenseList = (props) => {
+  const [filterSelection, setFilterSelection] = useState('2020');
+
+  const handleFilterChange = (selectedYear) => {
+    console.log(selectedYear, 'in ExpenseList');
+    setFilterSelection(selectedYear);
+  };
+
   return (
     <Card className='expenses'>
+      <ExpensesFilter
+        onFilterChange={handleFilterChange}
+        selected={filterSelection}
+      />
       {props.expenses.map((exp) => {
         return (
-          <ExpenseItem date={exp.date} title={exp.title} amount={exp.amount} />
+          <ExpenseItem
+            date={exp.date}
+            title={exp.title}
+            amount={exp.amount}
+            key={exp.id}
+          />
         );
       })}
     </Card>

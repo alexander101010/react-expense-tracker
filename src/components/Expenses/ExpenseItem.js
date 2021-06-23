5a4623d49add0243b945cae5b681fd4546a73b83
Alexander Botwinick <alexbotwinick@gmail.com>
2021-06-23 16:47:19 +0200

    updates to ExpenseItem component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 import './ExpenseItem.css'; // tells build process to consider this css
 
-const ExpenseItem = ({ title, amount, date }) => {
-  // using destructuring with props
-  // return (
-  //   <div className='expense-item'>
-  //     <ExpenseDate date={props.date} />
-  //     <div className='expense-item__description'>
-  //       <h2>{props.title}</h2>
-  //       <div className='expense-item__price'>{`${props.amount}€`}</div>
-  //     </div>
-  //   </div>
-  // );
+const ExpenseItem = (props) => {
+  // useState always returns an array with two values
+  // first value is pointing at the value, second is a function that can be called to change the value
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('New Title');
+    // using this function from useState schedules a reevaluation and update of component
+    console.log(title);
+  };
+
   return (
     <Card className='expense-item'>
-      <ExpenseDate date={date} />
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
-        <div className='expense-item__price'>{`${amount}€`}</div>
+        <div className='expense-item__price'>{`${props.amount}€`}</div>
       </div>
+      <button className='displayNone' onClick={clickHandler}>
+        Change Title
+      </button>
     </Card>
   );
 };

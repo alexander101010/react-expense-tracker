b50e62dae713cde14058afbef070ea953033bed4
Alexander Botwinick <alexbotwinick@gmail.com>
2021-06-24 14:49:44 +0200

    item is now a list item because EList is a ul

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -17,16 +17,18 @@ const ExpenseItem = (props) => {
   };
 
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>{`${props.amount}€`}</div>
-      </div>
-      <button className='displayNone' onClick={clickHandler}>
-        Change Title
-      </button>
-    </Card>
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{title}</h2>
+          <div className='expense-item__price'>{`${props.amount}€`}</div>
+        </div>
+        <button className='displayNone' onClick={clickHandler}>
+          Change Title
+        </button>
+      </Card>
+    </li>
   );
 };
 

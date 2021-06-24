9e3831a5f584a07f282aa555be92590ab6bfeea6
Alexander Botwinick <alexbotwinick@gmail.com>
2021-06-24 14:52:07 +0200

    add state and logic to conditionally render new expense form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [formShow, setFormShow] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(), // good enough id for this demo but could have repeats theoretically, and the ids will be unordered
     };
-    // console.log(expenseData);
     props.onAddExpense(expenseData);
+    setFormShow(false);
   };
-  return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-    </div>
+
+  const cancelClickHandler = () => {
+    setFormShow(false);
+  };
+
+  const formButtonClickHandler = () => {
+    setFormShow(true);
+  };
+
+  const form = (
+    <ExpenseForm
+      onSaveExpenseData={saveExpenseDataHandler}
+      onCancel={cancelClickHandler}
+    />
   );
+
+  const formButton = (
+    <button onClick={formButtonClickHandler}>Add New Expense</button>
+  );
+
+  return <div className='new-expense'>{formShow ? form : formButton}</div>;
 };
 
 export default NewExpense;

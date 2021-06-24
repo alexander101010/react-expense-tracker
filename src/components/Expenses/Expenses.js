357701aa19fb57d01bf8d9a5da1fe6fc0e452a2e
Alexander Botwinick <alexbotwinick@gmail.com>
2021-06-24 14:48:01 +0200

    changed file name from ExpensesList to Expenses to make room for expensesList component that is JUST the list

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import Card from '../UI/Card';
+
+import './Expenses.css';
+
+const Expenses = ({ expenses }) => {
+  const [filterSelection, setFilterSelection] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilterSelection(selectedYear);
+  };
+
+  const filteredExpenses = expenses.filter(
+    (exp) => exp.date.getFullYear() === parseInt(filterSelection, 10)
+  );
+
+  return (
+    <Card className='expenses'>
+      <ExpensesFilter
+        onFilterChange={filterChangeHandler}
+        selected={filterSelection}
+      />
+      <ExpensesList items={filteredExpenses} />
+    </Card>
+  );
+};
+
+export default Expenses;
